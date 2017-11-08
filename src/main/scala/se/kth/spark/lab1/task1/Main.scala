cf65b77dffc37a6a400edd5e146adc867c3ee32f
hadisotudeh <hadisotudeh1992@gmail.com>
2017-11-08 12:25:31 +0100

    Task1 finished

--- src/main/scala/se/kth/spark/lab1/task1/Main.scala
+++ src/main/scala/se/kth/spark/lab1/task1/Main.scala
@@ -7,8 +7,9 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._
 
-case class Song(yaer: Double, f1: Double, f2: Double, f3: Double)
+case class Song(year: Double, f1: Double, f2: Double, f3: Double)
 
 object Main {
   def main(args: Array[String]) {
@@ -22,18 +23,29 @@ object Main {
     val filePath = "src/main/resources/millionsong.txt"
 
     val rdd = sc.textFile(filePath)
-    val rawDF = rdd.toDF("raw").cache()
     //Step1: print the first 5 rows, what is the delimiter, number of features and the data types?
-    rawDF.take(5).foreach(println)
-
+//    rdd.take(5).foreach(println)
+    
     //Step2: split each row into an array of features
     val recordsRdd = rdd.map(s=>s.split(","))
     
     //Step3: map each row into a Song object by using the year label and the first three features  
     val songsRdd = recordsRdd.map(s => Song(s(0).toDouble,s(1).toDouble,s(2).toDouble,s(3).toDouble))
-
-    //Step4: convert your rdd into a datafram
+    //Step4: convert your rdd into a dataframe
     val songsDf = songsRdd.toDF()
 //    songsDf.collect().foreach(println)
+//    songsDf.show()
+//    Q1
+    println(songsDf.count()) 
+//    Q2
+    val yearCol= col("year")
+    songsDf.groupBy("year").count().filter(yearCol<=2000 && yearCol>=1998).agg(sum(col("count"))).show()
+//    Q3
+    songsDf.select(min(yearCol)).show()
+    songsDf.select(max(yearCol)).show()
+    songsDf.select(avg(yearCol)).show()
+//    Q4
+    songsDf.groupBy("year").count().filter(yearCol<=2010 && yearCol>=2000).sort(col("year")).show()
+
   }
 }
